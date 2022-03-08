0e3488093fa150ed6d53466edd57f7b36c0161e4
EfeYgt <95014290+EfeYgt@users.noreply.github.com>
2022-03-08 17:09:33 +0300

    crypto

---
+++ app/src/main/java/com/example/cryptocrazyapp/util/Constants.kt
@@ -0,0 +1,9 @@
+package com.example.cryptocrazyapp.util
+
+object Constants {
+
+    const val BASE_URL = "https://api.nomics.com/v1/"
+    const val API_KEY = "d5e75d5855d1801bfb907194cfd92caafb1e00e0"
+    const val CALL_ATTRIBUTES = "id,name,logo_url"
+
+}
